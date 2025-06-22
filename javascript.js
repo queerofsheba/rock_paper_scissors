dd53d9ca5c71c98c8472348bafc6501b24eea736
Ruby <Home@RBMacBookPro.attlocal.net>
2025-06-22 15:46:35 -0700

    Create function to attain computer choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
 console.log("Hello, Jade GC!")
+
+function getComputerChoice() {
+// CREATE three choices - rock, paper, & scissors
+    let choices = ["rock", "paper", "scissors"]; // create array of three options
+// SELECT choice at random
+    let number = Math.floor(Math.random() * 3); // stores random number from 0-2
+// RETURN randomly selected choice
+    return choices[number]; // returns option from choices indexed at number
+}
+
+console.log(getComputerChoice())
+console.log(getComputerChoice())
+console.log(getComputerChoice())
