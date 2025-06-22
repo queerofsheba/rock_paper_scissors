a30525daec8761c7df5281ecfeb5e8a440c252e3
Ruby <Home@RBMacBookPro.attlocal.net>
2025-06-22 16:27:22 -0700

    Create function to assess human choice

--- javascript.js
+++ javascript.js
@@ -4,11 +4,24 @@ function getComputerChoice() {
 // CREATE three choices - rock, paper, & scissors
     let choices = ["rock", "paper", "scissors"]; // create array of three options
 // SELECT choice at random
-    let number = Math.floor(Math.random() * 3); // stores random number from 0-2
+    let number = Math.floor(Math.random() * 3); // store random number from 0-2
 // RETURN randomly selected choice
-    return choices[number]; // returns option from choices indexed at number
+    return choices[number]; // return option from choices indexed at number
 }
 
-console.log(getComputerChoice())
-console.log(getComputerChoice())
-console.log(getComputerChoice())
+function getHumanChoice() {
+// PROMPT user to input choice    
+    let choice = prompt("Rock, paper, or scissors?"); // store user answer in variable
+
+// RESPOND to user choice, assuming it is valid    
+    if (choice.toLowerCase() === "rock") {
+        return "You choose rock!" // return message for rock
+    } else if (choice.toLowerCase() === "paper") {
+        return "You choose paper!" // return message for paper
+    } else if (choice.toLowerCase() === "scissors") {
+        return "You choose scissors!" // return message for scissors
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getHumanChoice());
