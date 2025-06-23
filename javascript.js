ad38b05321d330259b93ff6d5b37a31cb23a84c5
Ruby <Home@RBMacBookPro.attlocal.net>
2025-06-22 17:59:49 -0700

    Create logic for entire game

--- javascript.js
+++ javascript.js
@@ -6,6 +6,7 @@ function getComputerChoice() {
 // SELECT choice at random
     let number = Math.floor(Math.random() * 3); // store random number from 0-2
 // RETURN randomly selected choice
+    console.log("Computer chooses " + choices[number]);
     return choices[number]; // return option from choices indexed at number
 }
 
@@ -15,42 +16,67 @@ function getHumanChoice() {
 
 // RESPOND to user choice, assuming it is valid    
     if (choice.toLowerCase() === "rock") {
-        return "You choose rock!" // return message for rock
+        console.log("You choose rock!") // return message for rock
     } else if (choice.toLowerCase() === "paper") {
-        return "You choose paper!" // return message for paper
+        console.log("You choose paper!") // return message for paper
     } else if (choice.toLowerCase() === "scissors") {
-        return "You choose scissors!" // return message for scissors
+        console.log("You choose scissors!") // return message for scissors
     }
+// CLOSE function and return user answer    
+    return choice
 }
 
 let humanScore = 0 // create variable for human score
 let computerScore = 0 // create variable for computer score
 
-function playRound(humanChoice, computerChoice) {
-    if (humanChoice.toLowerCase() === computerChoice) {
-        return "It's a draw!"
-    } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "paper") {
-        computerScore += 1
-        return "You lose! Paper beats rock."
-    } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
-        humanScore += 1
-        return "You win! Rock beats scissors."
-    } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") {
-        humanScore += 1
-        return "You win! Paper beats rock."
-    } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "scissors") {
-        computerScore += 1
-        return "You lose! Scissors beat paper."
-    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "rock") {
-        computerScore += 1
-        return "You lose! Rock beats scissors."
-    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
-        humanScore += 1
-        return "You win! Scissors beat paper."
+function playGame () {
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice.toLowerCase() === computerChoice) {
+            return "It's a draw!"
+        } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "paper") {
+            computerScore += 1
+            return "You lose! Paper beats rock."
+        } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
+            humanScore += 1
+            return "You win! Rock beats scissors."
+        } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") {
+            humanScore += 1
+            return "You win! Paper beats rock."
+        } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "scissors") {
+            computerScore += 1
+            return "You lose! Scissors beat paper."
+        } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "rock") {
+            computerScore += 1
+            return "You lose! Rock beats scissors."
+        } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+            humanScore += 1
+            return "You win! Scissors beat paper."
+        }
     }
+
+let roundOne = playRound(getHumanChoice(), getComputerChoice())
+console.log("Human: " + humanScore)
+console.log("Computer: " + computerScore)
+let roundTwo = playRound(getHumanChoice(), getComputerChoice())
+console.log("Human: " + humanScore)
+console.log("Computer: " + computerScore)
+let roundThree = playRound(getHumanChoice(), getComputerChoice())
+console.log("Human: " + humanScore)
+console.log("Computer: " + computerScore)
+let roundFour = playRound(getHumanChoice(), getComputerChoice())
+console.log("Human: " + humanScore)
+console.log("Computer: " + computerScore)
+let roundFive = playRound(getHumanChoice(), getComputerChoice())
+console.log("Final score - Human " + humanScore + ", Computer " + computerScore)
+if (humanScore > computerScore) {
+    console.log("Congrats best friend, you did that!")
+} else if (humanScore < computerScore) {
+    console.log("Tough luck, bookie, the computer is smarter.")
+} else if (humanScore === computerScore) {
+    console.log("Victor undetermined, new game suggested")
+}
+// console.log("Human: " + humanScore)
+// console.log("Computer: " + computerScore)
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
-console.log(playRound("paper", "rock"));
-console.log(humanScore, computerScore);
+console.log(playGame())
